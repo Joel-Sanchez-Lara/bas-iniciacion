836d542838667412edf443ca7950fcd5d796cc7e
joel-sanchez-lara <jsanchez1502@gmail.com>
2021-09-10 03:19:51 +0000

    Primer commit en dev

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("jsbas.jsbas1.controller.Main", {
+			onInit: function () {
+
+			}
+		});
+	});
